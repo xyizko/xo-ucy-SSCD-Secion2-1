555275ceb692e9f02aa11fe22c5c8bce5e5d4b4b
xyizko <xyizko@proton.me>
2025-03-05 23:32:38 +0530

    - StorageFactory Notes

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,3 +1,7 @@
+// Based on the tutorial this contrat is deploying other contracts
+//
+//
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
@@ -8,12 +12,14 @@ import {SimpleStorage} from "./SimpleStorage.sol";
 contract StorageFactory {
     SimpleStorage[] public listOfSimpleStorageContracts;
 
+    // This is the function that is the orange button when deploying 
     function createSimpleStorageContract() public {
         SimpleStorage simpleStorageContractVariable = new SimpleStorage();
         // SimpleStorage simpleStorage = new SimpleStorage();
         listOfSimpleStorageContracts.push(simpleStorageContractVariable);
     }
 
+    // Storage Factor Store
     function sfStore(
         uint256 _simpleStorageIndex,
         uint256 _simpleStorageNumber
@@ -26,6 +32,7 @@ contract StorageFactory {
         );
     }
 
+    // sfGET = Storage Factory Get
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
         // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
         return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
